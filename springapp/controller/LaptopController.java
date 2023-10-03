f55e470ae795163e194825652d3504214fe4fd77
Iamneo <techadmin@iamneo.ai>
2023-10-03 13:14:48 +0000

    Backup Commit

---
+++ springapp/controller/LaptopController.java
@@ -0,0 +1,7 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/laptop")
+public class LaptopController{
+    
+}
