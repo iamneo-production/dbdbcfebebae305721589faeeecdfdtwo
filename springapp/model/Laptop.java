a0232baac38b0523f4a6e7a1c45b43d66dcb2fb8
Iamneo <techadmin@iamneo.ai>
2023-10-03 13:09:15 +0000

    Backup Commit

--- springapp/model/Laptop.java
+++ springapp/model/Laptop.java
@@ -4,6 +4,25 @@ public class Laptop{
     private String laptopBrand;
 
     int getLaptopId(){
-        return lap
+        return laptopId;
+    }
+    int getLaptopPrice(){
+        return laptopPrice;
+    }
+    String getLaptopBrand(){
+        return laptopBrand;
+    }
+
+    void setLaptopId(int laptopId){
+        this.laptopId=laptopId;
+    }
+
+    void setLaptopPrice(int laptopPrice){
+        this.laptopPrice=laptopPrice;
+    }
+
+    void setLaptopBrand(String laptopBrand){
+        this.laptopBrand=laptopBrand;
     }
 }
+
